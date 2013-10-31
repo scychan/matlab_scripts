d5cbf94e44314a50831279f3289ebd504f1064c5
Stephanie Chan <scychan@princeton.edu>
2013-10-31 17:52:30 -0400

    initnans - now accepts any size param (not restricted to 2-D)

--- vectors and matrices/initnans.m
+++ vectors and matrices/initnans.m
@@ -1,7 +1,14 @@
-function [] = initnans(variable_names,m,n)
+function [] = initnans(variable_names,arraysize)
 % function [] = initnans(variable_names,m,n)
 
 nvars = length(variable_names);
+
+arraysizestr = '';
+for dim = 1:length(arraysize)
+    arraysizestr = sprintf('%s,%i',arraysizestr,arraysize(dim));
+end
+arraysizestr = arraysizestr(2:end);
+
 for v = 1:nvars
-    evalin('caller',sprintf('%s = nan(%i,%i);',variable_names{v},m,n));
+    evalin('caller',sprintf('%s = nan(%s);',variable_names{v},arraysizestr));
 end
