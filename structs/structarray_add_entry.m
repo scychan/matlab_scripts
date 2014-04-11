f3494fce0441d369a944b58b7657892bad925453
Stephanie Chan <scychan@princeton.edu>
2014-04-11 15:09:38 -0400

    structs/structarray_add_entry.m - updates

--- structs/structarray_add_entry.m
+++ structs/structarray_add_entry.m
@@ -1,10 +1,21 @@
-function newarray = structarray_add_entry(origarray,arrayind,entry)
-% function newarray = structarray_add_entry(origarray,structind,entry)
+function newarray = structarray_add_entry(origarray,arrayind,entry,samefieldsrequired)
+% function newarray = structarray_add_entry(origarray,structind,entry,[samefieldsrequired])
 % 
 % Similar to origarray(structind) = entry, but allows entry to have
 % different/more fields than origarray
 % 
 % If origarray is 1-D, arrayind > length(origarray) is OK.
+
+if ~exist('samefieldsrequired','var')
+    samefieldsrequired = 1;
+end
+
+if samefieldsrequired
+    if ~all(strcmp(sort(fieldnames(origarray)),...
+            sort(fieldnames(entry))))
+        error('Fieldnames not the same for origarray and entry')
+    end
+end
 
 newarray = origarray;
 
