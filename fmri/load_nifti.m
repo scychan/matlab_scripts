65bc6703de1089a07728ab5e292d571f652afdd6
Stephanie Chan <scychan@princeton.edu>
2014-05-03 14:22:22 -0400

    fmri/load_nifti.m - allow loading of gzip files (.nii.gz)

--- fmri/load_nifti.m
+++ fmri/load_nifti.m
@@ -1,6 +1,13 @@
-function [V XYZ] = load_nifti(filename)
+function [V, XYZ] = load_nifti(filename)
 % function [V XYZ] = load_nifti(filename)
 
+% gunzip if necessary
+if strcmp(filename(end-2:end),'.gz')
+    copyfile(filename,'/tmp/tempnifti.nii.gz');
+    gunzip('/tmp/tempnifti.nii.gz');
+    filename = '/tmp/tempnifti.nii';
+end
+    
 vol = spm_vol(filename);
 
-[V XYZ] = spm_read_vols(vol);
+[V, XYZ] = spm_read_vols(vol);
