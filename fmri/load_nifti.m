bfcbbb1125faedcfab859fdb429e8fb72ab770b0
Stephanie Chan <scychan@princeton.edu>
2014-05-04 15:24:28 -0400

    fmri/load_nifti.m - when loading gzip files, use random filename in /tmp, and   delete the /tmp files when done

--- fmri/load_nifti.m
+++ fmri/load_nifti.m
@@ -3,11 +3,15 @@ function [V, XYZ] = load_nifti(filename)
 
 % gunzip if necessary
 if strcmp(filename(end-2:end),'.gz')
-    copyfile(filename,'/tmp/tempnifti.nii.gz');
-    gunzip('/tmp/tempnifti.nii.gz');
-    filename = '/tmp/tempnifti.nii';
+    tempname = sprintf('tempnifti%i',round(rand*100000));
+    copyfile(filename,sprintf('/tmp/%s.nii.gz',tempname));
+    gunzip(sprintf('/tmp/%s.nii.gz',tempname));
+    filename = sprintf('/tmp/%s.nii',tempname);
 end
     
 vol = spm_vol(filename);
 
 [V, XYZ] = spm_read_vols(vol);
+
+delete(filename)
+delete([filename '.gz'])
