de1595ead2289e4217bb77f4c57648cf957e25a8
Stephanie Chan <scychan@princeton.edu>
2014-09-16 10:33:30 -0400

    fmri/load_nifti.m - set random seed with job ID, if Della

--- fmri/load_nifti.m
+++ fmri/load_nifti.m
@@ -3,6 +3,18 @@ function [V, XYZ] = load_nifti(filename)
 
 % gunzip if necessary
 if strcmp(filename(end-2:end),'.gz')
+    if isdella
+        try
+            seed = getenv('SLURM_JOBID');
+        catch
+            seed = getenv('SLURM_ARRAY_JOB_ID') * getenv('SLURM_ARRAY_TASK_ID');
+        end
+        rand('twister', seed)
+    elseif isrondo
+        error('need to write this')
+    else
+        setseedwclock
+    end
     tempname = sprintf('tempnifti%i',round(rand*100000));
     copyfile(filename,sprintf('/tmp/%s.nii.gz',tempname));
     gunzip(sprintf('/tmp/%s.nii.gz',tempname));
