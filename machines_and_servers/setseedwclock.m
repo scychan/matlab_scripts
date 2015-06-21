e25e43d384b92960d5bec1565dd13af0588cdbb9
Stephanie Chan <scychan@princeton.edu>
2015-06-21 14:19:19 -0400

    setseedwclock -- only show seed if 'v' (verbose)

--- machines_and_servers/setseedwclock.m
+++ machines_and_servers/setseedwclock.m
@@ -1,7 +1,10 @@
+function setseedwclock(verbose)
 % sets the seed for RandStream using the clock, so that results are
 % different for each run
 
 seed = sum(100*clock);
-fprintf('Random seed is: %f\n',seed)
+if exist('verbose','var') & strcmp(verbose,'v')
+    fprintf('Random seed is: %f\n',seed)
+end
 % RandStream.setDefaultStream(RandStream('mt19937ar','seed',seed));
 rand('twister', seed)
