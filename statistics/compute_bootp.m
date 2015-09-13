1be3dc7a0eef0759aa421ddee60ee81b3d72b78a
Stephanie Chan <scychan@princeton.edu>
2015-09-12 23:18:26 -0400

    new file:   statistics/compute_bootp.m

---
+++ statistics/compute_bootp.m
@@ -0,0 +1,18 @@
+function [bootp, bootsamples] = compute_bootp(data,greaterthan_or_lessthan,threshold,nboot)
+% function [bootp, bootsamples] = compute_bootp(data,greaterthan_or_lessthan,threshold[,nboot])
+% 
+% default nboot = 10,000
+
+N = length(data);
+if ~exist('nboot','var')
+    nboot = 10000;
+end
+
+sel = squeeze(RandSel(1:N,[nboot N]));
+bootsamples = mean(data(sel),2);
+switch greaterthan_or_lessthan
+    case 'greaterthan'
+        bootp = sum(bootsamples < threshold)/nboot;
+    case 'lessthan'
+        bootp = sum(bootsamples > threshold)/nboot;
+end
