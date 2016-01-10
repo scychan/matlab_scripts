ba3f0d0677f699da36d3d8dd8eb5ba03e92e31ce
Stephanie Chan <scychan@princeton.edu>
2016-01-10 10:36:58 +0800

    statistics/compute_bootp.m - allow 2-sided

--- statistics/compute_bootp.m
+++ statistics/compute_bootp.m
@@ -1,5 +1,5 @@
-function [bootp, bootsamples] = compute_bootp(data,greaterthan_or_lessthan,threshold,nboot)
-% function [bootp, bootsamples] = compute_bootp(data,greaterthan_or_lessthan,threshold[,nboot])
+function [bootp, bootsamples] = compute_bootp(data,greaterthan_or_lessthan_or_twosided,threshold,nboot)
+% function [bootp, bootsamples] = compute_bootp(data,greaterthan_or_lessthan_or_twosided,threshold[,nboot])
 % 
 % NOTE: This is a one-sided test
 % 
@@ -12,9 +12,13 @@ end
 
 sel = squeeze(RandSel(1:N,[nboot N]));
 bootsamples = mean(data(sel),2);
-switch greaterthan_or_lessthan
+switch greaterthan_or_lessthan_or_twosided
     case 'greaterthan'
         bootp = sum(bootsamples < threshold)/nboot;
     case 'lessthan'
         bootp = sum(bootsamples > threshold)/nboot;
+    case 'twosided'
+        bootp = sum(bootsamples < threshold)/nboot;
+        if bootp > 0.5, bootp = 1-bootp; end
+        bootp = 2*bootp;
 end
