27b8f5503e3152a6d9dbb1bac0e2c57906fc8a84
Stephanie Chan <scychan@princeton.edu>
2016-01-10 10:36:13 +0800

    plots/plotregression.m - new option "showplot"

--- plots/plotregression.m
+++ plots/plotregression.m
@@ -1,4 +1,4 @@
-function [B,BINT,R,RINT,STATS] = plotregression(x,y,add_ones,colorcode_significance)
+function [B,BINT,R,RINT,STATS] = plotregression(x,y,add_ones,colorcode_significance,showplot)
 % function [B,BINT,R,RINT,STATS] = plotregression(x,y[,add_ones,colorcode_significance])
 % assumes x is one-dimensional
 
@@ -12,6 +12,10 @@ if ~exist('colorcode_significance','var')
     colorcode_significance = 1;
 end
 
+if ~exist('showplot','var')
+    showplot = 1;
+end
+
 %% regression
 
 if add_ones
@@ -22,13 +26,15 @@ end
 
 %% plot
 
-if colorcode_significance
-    if STATS(3) < 0.05
-        color = 'm';
-    else
-        color = 'b';
+if showplot
+    if colorcode_significance
+        if STATS(3) < 0.05
+            color = 'm';
+        else
+            color = 'b';
+        end
     end
+    
+    xlims = get(gca,'xlim');
+    plot(xlims,B(1)*xlims + B(2),color)
 end
-
-xlims = get(gca,'xlim');
-plot(xlims,B(1)*xlims + B(2),color)
