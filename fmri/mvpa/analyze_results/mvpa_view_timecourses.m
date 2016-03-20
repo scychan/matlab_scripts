b835898c2c9bc225a9b967aae2a3a196791af7b0
Stephanie Chan <scychan@princeton.edu>
2016-03-20 06:15:30 -0400

    mvpa_view_timecourses.m - allow intervals instead of single line

--- fmri/mvpa/analyze_results/mvpa_view_timecourses.m
+++ fmri/mvpa/analyze_results/mvpa_view_timecourses.m
@@ -3,19 +3,34 @@ function mvpa_view_timecourses(acts,desireds,timepoints)
 %
 % View classifier output timecourse for a single iteration.
 % 'timepoints' input is optional
+% 
+% acts - should be [ncategories x ntimepoints]
+%      - if wanting to view intervals (rather than a single line for each category),
+%        acts{1} should be the lower bounds, and acts{2} should be the upper bounds.
 
 hold on
 
+% figure out whether it is CI or means
+doCI = iscell(acts);
+if ~doCI, temp{1} = acts; acts = temp; end
+
 % basics
-[ncats ntimepts] = size(acts);
+[ncats ntimepts] = size(acts{1});
 colors = jet(ncats);
 if ~exist('timepoints','var')
     timepoints = 1:ntimepts;
 end
 
 % plot classifier activations
-for icat = 1:ncats
-    plot(timepoints,acts(icat,timepoints),'.-','color',colors(icat,:))
+if doCI
+    allcolors = 'bgm';
+    for icat = 1:ncats
+        fill_curve(allcolors(icat), 1:ntimepts, acts{1}(icat,:), acts{2}(icat,:));
+    end
+else
+    for icat = 1:ncats
+        plot(timepoints,acts{1}(icat,timepoints),'.-','color',colors(icat,:))
+    end
 end
 
 % plot dot labels just above 1
