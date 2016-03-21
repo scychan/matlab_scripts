51df7b7e3da4dd73d3dba7e7ae8a5a157ef61cb1
Stephanie Chan <scychan@princeton.edu>
2016-03-21 11:12:42 -0400

    get_git_id -- bugfix

--- git/get_git_id.m
+++ git/get_git_id.m
@@ -1,3 +1,4 @@
 function commit_id = get_git_id
 
-commit_id = unix('git --no-pager log -1 --format="%h"');
+[~,commit_id] = unix('git --no-pager log -n 1 --format="%h"');
+commit_id = commit_id(1:end-1); % remove the newline
